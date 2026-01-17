953f0ffeeb44c66638a448c0f480fd1c9d9c6c1b
Damien-Ruban <42767042+Damien-Ruban@users.noreply.github.com>
2026-01-17 20:20:29 +0000

    Update and rename 7. Reverse Integer to 7. Reverse Integer.py

--- problem-list/7. Reverse Integer.py
+++ problem-list/7. Reverse Integer.py
@@ -9,7 +9,7 @@ class Solution:
             return 0
         return x_reversed
         
-class Solution:
+# class Solution:
 #     def reverse(self, x: int) -> int:
 #         sign = 1 if x >= 0 else -1
 #         abs_x = abs(x)
